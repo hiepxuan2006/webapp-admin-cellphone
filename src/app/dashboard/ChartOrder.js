c26e7bb83253c614222d3388ee391ed31753f293
hiepxuan <hiepxuan2006@gmail.com>
2023-07-11 06:57:56 +0700

    update

--- src/app/dashboard/ChartOrder.js
+++ src/app/dashboard/ChartOrder.js
@@ -6,7 +6,26 @@ import BarChart from "../../helpers/BarChart"
 
 export const ChartOrder = () => {
   const [data, setData] = useState([])
-  const [userData, setUserData] = useState({})
+  const [userData, setUserData] = useState({
+    labels: data.map((item) => item.time),
+    datasets: [
+      {
+        label: "Order",
+        data: data.map((item) => item.totalAmount),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+          "#2a71d0",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  })
   const _getAnalyticOrder = async () => {
     const { success, data, message } = await analyticOrder()
     if (!success) {
@@ -40,5 +59,9 @@ export const ChartOrder = () => {
         ],
       })
   }, [data])
-  return <div>{data.length && <BarChart chartData={userData} />}</div>
+  return (
+    <div>
+      <BarChart chartData={userData} />
+    </div>
+  )
 }
